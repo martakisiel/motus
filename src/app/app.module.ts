adbbfe901f86b247b4f89b7eb1413e4e9c0925c8
marta <marta.kisiel91@gmail.com>
2023-07-17 22:30:30 +0200

    new menu with doprdown list that collapse, based on bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeBannerComponent } from './welcome-banner/welcome-banner.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeBannerComponent
+    WelcomeBannerComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
