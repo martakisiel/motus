cc2df21350ba8d82847cf8862fa6afc08f8fbc79
marta <marta.kisiel91@gmail.com>
2023-10-21 22:32:48 +0200

    file with aktualnosci.txt was added and connected to actualnosci.component via service.service

--- src/app/aktualnosci/aktualnosci.component.ts
+++ src/app/aktualnosci/aktualnosci.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ServiceService, Aktualnosci } from '../service.service';
 @Component({
   selector: 'app-aktualnosci',
   templateUrl: './aktualnosci.component.html',
   styleUrls: ['./aktualnosci.component.css']
 })
-export class AktualnosciComponent {
+export class AktualnosciComponent implements OnInit {
 
+  aktualnosci: Aktualnosci[] = []; // Make sure data is of type string
+  constructor(private serviceService: ServiceService) {}
+
+  ngOnInit() {
+    this.serviceService.getAktualnosciFile().subscribe((response: string) => {
+      this.aktualnosci = JSON.parse(response) as Aktualnosci[];
+      // console.log(this.aktualnosci); 
+    });
+  }
 }
--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,17 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
   private selectedImageIndex = 0;
-  constructor() { }
+  constructor(private http: HttpClient) { }
   private logoSponsors: Sponsors[] = [
     { alt: "SkyWorldGames", url: "assets/sponsorzy/1.png", link: "https://skyworldgames.com/" },
     { alt: "gmina Lesko", url: "assets/sponsorzy/2.jpg", link: "https://www.lesko.pl/" },
 
   ]
 
+  // private aktualnosci: Aktualnosci[] = [
+  //   { title: "Mecz siatkówki na hali SP Lesko", date: "21.09.2023", content:"W czwartek 21.09.2023 o godzinie 19:00 odbędzie się koleżeński mecz siatkówki."},
+  //   { title: "assets/galerySample/gs2.JPG", date: "10.10.2023", content:"Mecz na hali" },
+  //   { title: "assets/galerySample/gs3.JPG", date: "10.10.2023", content:"Mecz na hali" },
+  //   { title: "assets/galerySample/gs4.JPG", date: "10.10.2023", content:"Mecz na hali" },
+  //   { title: "assets/galerySample/gs5.JPG", date: "10.10.2023", content:"Mecz na hali" }
+
+  // ]
   private galerySample: GalerySample[] = [
     { url: "assets/galerySample/gs1.JPG", alt: "San" },
     { url: "assets/galerySample/gs2.JPG", alt: "serw" },
@@ -21,8 +30,12 @@ export class ServiceService {
 
   ]
 
-  //DOROBIC TABLICE Z AKTUALNOSCIAMI
-  
+
+  getAktualnosciFile(): Observable<string> {
+    return this.http.get('assets/teksty/aktualnosci.txt', { responseType: 'text' });
+  }
+
+
   getLogoSponsors() {
     return this.logoSponsors;
   }
@@ -48,4 +61,11 @@ export interface GalerySample {
   alt: string | undefined;
   url: string;
   //   link: string;
+}
+export interface Aktualnosci {
+
+  title: string;
+  date: string ;
+  content: string;
+
 }
