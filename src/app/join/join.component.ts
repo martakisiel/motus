ace20607fb483528015618ad615fd8071e0498b9
marta <marta.kisiel91@gmail.com>
2023-11-28 23:05:58 +0100

    join.component fixed

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.css']
 })
 export class JoinComponent {
-  constructor(private route:ActivatedRoute){}
+  message: string | undefined;
+
+  constructor(private route:ActivatedRoute,private http: HttpClient, private clipboard: Clipboard, public matSnackBar:MatSnackBar){}
+
+  copyText(textToCopy: string){
+    this.clipboard.copy(textToCopy);
+  }
+  openSnackBar(message:string, action:string){
+    this.matSnackBar.open(message, action, { duration: 2000});
+    }
+  showNotification() {
+    this.openSnackBar('Skopiowano do schowka', 'zamknij');
+  }
 }
--- src/app/sekcje/sekcje.component.ts
+++ src/app/sekcje/sekcje.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sekcje.component.css']
 })
 export class SekcjeComponent {
+constructor() { }
+
 
 }
