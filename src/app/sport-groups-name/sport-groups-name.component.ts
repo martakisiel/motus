53a0fab5c610f4f8c24bc38c71b5e494eedf5637
marta <marta.kisiel91@gmail.com>
2023-12-11 14:38:05 +0100

    new component for routing?

---
+++ src/app/sport-groups-name/sport-groups-name.component.ts
@@ -0,0 +1,10 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sport-groups-name',
+  templateUrl: './sport-groups-name.component.html',
+  styleUrls: ['./sport-groups-name.component.css']
+})
+export class SportGroupsNameComponent {
+
+}
