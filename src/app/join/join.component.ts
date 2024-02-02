a5fb8a89a21d569352f849478de43c39a1e78cd7
marta <marta.kisiel91@gmail.com>
2024-02-02 10:29:48 +0100

    new version of join.component

--- src/app/join/join.component.ts
+++ src/app/join/join.component.ts
@@ -3,23 +3,32 @@ import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Clipboard } from '@angular/cdk/clipboard';
+import * as AOS from 'aos';
+
 @Component({
   selector: 'app-join',
   templateUrl: './join.component.html',
   styleUrls: ['./join.component.css']
 })
 export class JoinComponent {
-  message: string | undefined;
-
-  constructor(private route:ActivatedRoute,private http: HttpClient, private clipboard: Clipboard, public matSnackBar:MatSnackBar){}
-
-  copyText(textToCopy: string){
-    this.clipboard.copy(textToCopy);
-  }
-  openSnackBar(message:string, action:string){
-    this.matSnackBar.open(message, action, { duration: 2000});
-    }
-  showNotification() {
-    this.openSnackBar('Skopiowano do schowka', 'zamknij');
-  }
+  ngOnInit() { 
+    AOS.init({
+    
+    });
+      }
+      message: string | undefined;
+      constructor(private http: HttpClient, private clipboard: Clipboard, public matSnackBar:MatSnackBar) { }
+      
+      copyText(textToCopy: string) {
+        this.clipboard.copy(textToCopy);
+        }
+        openSnackBar(message: string, action: string) {
+          this.matSnackBar.open(message, action, {
+            duration: 2000, // Duration in milliseconds
+          });
+        }
+        //metoda wywołująca 'skopowano do schowka'
+        showNotification() {
+          this.openSnackBar('Skopiowano do schowka', 'zamknij');
+        }
 }
